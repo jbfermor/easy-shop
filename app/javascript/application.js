5c10f038a7bfa0ac833983beb6d8cad4b64f7e88
jbfermor <jbfermor@hotmail.com>
2024-02-06 15:45:34 +0100

    init + bootstrap + uuid + tests

---
+++ app/javascript/application.js
@@ -0,0 +1,9 @@
+// Entry point for the build script in your package.json
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+
